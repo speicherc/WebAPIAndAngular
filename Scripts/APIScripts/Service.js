e6bd585b79b4c47a5b7d0312c42148fe3092521f
speicherc <carl.e.speicher@saic.com>
2017-07-27 16:31:10 -0700

    additions to initial library
    additions to initial library

---
+++ Scripts/APIScripts/Service.js
@@ -0,0 +1,33 @@
+﻿app.service("APIService", function ($http) {
+    this.getSubs = function () {
+        return $http.get("api/Subscriber")
+    }
+
+    this.saveSubscriber = function (sub) {
+        return $http(
+        {
+            method: 'post',
+            data: sub,
+            url: 'api/Subscriber'
+        });
+    }
+
+    this.updateSubscriber = function (sub) {
+        return $http(
+        {
+            method: 'put',
+            data: sub,
+            url: 'api/Subscriber'
+        });
+    }
+
+    this.deleteSubscriber = function (subID) {
+        var url = 'api/Subscriber/' + subID;
+        return $http(
+        {
+            method: 'delete',
+            data: subID,
+            url: url
+        });
+    }
+});
